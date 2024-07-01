0b22587d4679a9386fdcd57e9d0c6c28b2e6a754
kyvi721997 <kyvi721997@gmail.com>
2024-07-01 21:06:58 +0700

    Update page

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 import 'react-h5-audio-player/lib/styles.css';
 import HomePage from "src/pages/HomePage";
-import {Route, Routes} from "react-router-dom";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 import WelcomePage from "src/pages/WelcomePage";
 
 function App() {
     return (
-        <>
-            <Routes>
-                <Route path='/' element={<WelcomePage/>} />
-                <Route path='/home' element={<HomePage/>} />
-            </Routes>
-        </>
+        <BrowserRouter>
+                <Routes>
+                    <Route path='/' element={<WelcomePage/>}/>
+                    <Route path='/home' element={<HomePage/>}/>
+                </Routes>
+        </BrowserRouter>
     )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,12 @@ import App from './App.tsx'
 import './index.css';
 import 'assets/scss/font.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {BrowserRouter} from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
 root.render(
     <React.StrictMode>
-        <BrowserRouter>
+        {/*<BrowserRouter>*/}
             <App/>
-        </BrowserRouter>
+        {/*</BrowserRouter>*/}
     </React.StrictMode>,
 )
